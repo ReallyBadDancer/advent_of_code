0c30177b6f9dac99e4b0684a49899efe547f2a40
jsoares <jason.soares@viasat.com>
2022-12-01 14:20:48 -0800

    First day completed.

---
+++ 2022/1/most_calories.py
@@ -0,0 +1,24 @@
+
+def main():
+    with open("puzzle_input_01.txt", mode='r') as ifile:
+        snack_list = ifile.read().split('\n')
+
+    elf_inventories = []
+    elf_cals = 0
+    for snack in snack_list:
+        if snack:
+            elf_cals += int(snack)
+        else:
+            elf_inventories.append(elf_cals)
+            elf_cals = 0
+
+    top_three = 0
+    for i in range(3):
+        print(max(elf_inventories))
+        top_three += max(elf_inventories)
+        elf_inventories.remove(max(elf_inventories))
+
+    print(top_three)
+
+if __name__ == "__main__":
+    main()
