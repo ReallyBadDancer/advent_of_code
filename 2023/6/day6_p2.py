523dfac0b3b08ba0f14f3ed3cbf50c4e4ec3a07b
Jason Soares <jason.allen.soares@gmail.com>
2024-01-05 15:26:40 -0800

    Finished day 6 part 2.

--- 2023/6/day6_p2.py
+++ 2023/6/day6_p2.py
@@ -5,35 +5,21 @@ print("\nImport Raw data:")
 with open(ifilename) as ifile:
     data = ifile.read()
 print(data)
-
-print("\nConvert raw data into time/distance pairs")
+print("\nConvert raw data into single large time and distance integer")
 data = data.splitlines()
-data = [list(map(int, line.split(":")[1].split())) for line in data]  # Creates a list of integers
-print("Data as a list: ", data)
-time_distance_pairs = list(zip(*data))
-print("Data as time/distance pairs:", time_distance_pairs)
+print(data)
+data = [int(line.split(":")[1].replace(" ","")) for line in data]
+print("Time/Distance Data a blobbed integer: ", data)
 
 print("\nDetermine how long we must press the button to win the race: ")
-winning_sets = []
-for race_duration, best_distance in time_distance_pairs:
-    winning_press_times = []
-    for t in range(1, race_duration+1):
-        speed = t
-        run_duration = race_duration - t
-        distance = speed * run_duration
-        if distance > best_distance:
-            winning_press_times.append(t)
-    winning_sets.append(winning_press_times)
-print("Winning Sets: ", winning_sets)
+race_duration, best_distance = data
+winning_press_times = 0
+for t in range(1, race_duration+1):
+    print(f"Processing {t} of {race_duration}")
+    speed = t
+    run_duration = race_duration - t
+    distance = speed * run_duration
+    if distance > best_distance:
+        winning_press_times += 1
 
-print("\nCalculate number of possible winning combinations:")
-
-ways_to_win = 1
-for s in winning_sets:
-    ways_to_win *= len(s)
-
-print("Part 1 solution: ", ways_to_win)
-
-
-
-
+print("Part 2 solution: ", winning_press_times)
